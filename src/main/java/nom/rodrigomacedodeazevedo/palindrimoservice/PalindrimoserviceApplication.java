293a6d0b547eea005f9be70df26541ca38aedbc9
Rodrigo Macedo de Azevedo <rodrigo.kal@gmail.com>
2018-09-03 10:20:34 -0300

    Commit inicial - projeto gerado com spring boot initializr

---
+++ src/main/java/nom/rodrigomacedodeazevedo/palindrimoservice/PalindrimoserviceApplication.java
@@ -0,0 +1,12 @@
+package nom.rodrigomacedodeazevedo.palindrimoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindrimoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindrimoserviceApplication.class, args);
+	}
+}
